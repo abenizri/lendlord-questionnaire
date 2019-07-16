235203d394824da5f38b5cfd49a9f2d7d730ac72
nir <nshabi@wiser.com>
2019-07-16 14:12:24 +0300

    nir

--- src/components/result.jsx
+++ src/components/result.jsx
@@ -106,6 +106,43 @@ class Result extends Component {
       </React.Fragment>
     )
     if(!isMobile) {
+      productsContent = this.props.amount.includes("criteria") ?
+      (
+        <React.Fragment>
+          <Col sm="5">
+            <FormGroup style={{marginBottom: '0'}}>
+              <div>
+                <label style={{marginBottom: '0px', fontWeight: 700, fontSize: '14px'}}>{this.props.lender}</label>
+              </div>
+              <div>
+                <Row style={{ width: '100%', fontSize: '8px'}}>
+                  <Col sm="12" style={{paddingLeft: '16px'}}>{this.renderCriteriaMessages()}</Col>
+                </Row>
+              </div>
+            </FormGroup>
+          </Col>
+          <Col sm="6" style={{position: 'relative', fontSize: '12px'}}>{this.renderAmount()}</Col>
+        </React.Fragment>
+      ) :
+      (
+        <React.Fragment>
+          <Col sm="8">
+            <FormGroup style={{marginBottom: '0'}}>
+              <div>
+                <label style={{marginBottom: '0px', fontWeight: 700, fontSize: '14px'}}>{this.props.lender}</label>
+              </div>
+              <div>
+                <Row style={{ width: '100%', fontSize: '8px'}}>
+                  <Col xs={{size: 3}} style={{ paddingRight: '2px', paddingLeft: '16px'}}>{this.renderProduct()}</Col>
+                  <Col sm="3" style={{paddingRight: '2px', paddingLeft: '2px'}}>{this.renderRate()}</Col>
+                  <Col sm="3" style={{paddingRight: '2px', paddingLeft: '2px'}}>{this.renderReturn()}</Col>
+                </Row>
+              </div>
+            </FormGroup>
+          </Col>
+          <Col sm="3" style={{position: 'relative', fontSize: '12px'}}>{this.renderAmount()}</Col>
+        </React.Fragment>
+      )
       return (
         <FormGroup row type="result"  style={{ backgroundColor: '#f0f3f5', marginBottom: '0.3rem',borderRadius: '0.5rem', marginRight: '0px', marginLeft: '0px'}}>
           <Row style={{ width: '100%'}}>
@@ -114,22 +151,7 @@ class Result extends Component {
               <img alt={this.props.logo} src={"http://app.ex4solutions.com/images/" + this.props.logo + ".png"} width="30px" height="30px"/>
             </div>
             </Col>
-            <Col sm="8">
-              <FormGroup style={{marginBottom: '0'}}>
-                <div>
-                  <label style={{marginBottom: '0px', fontWeight: 700}}>{this.props.lender}</label>
-                </div>
-                <div>
-                  <Row style={{ width: '100%'}}>
-                    <Col sm={{size: 3, offset: 1}} style={{ paddingRight: '0px'}}>{this.renderProduct()}</Col>
-                    <Col sm="3" style={{paddingLeft: '0px'}}>{this.renderRate()}</Col>
-                    <Col sm="3" style={{paddingLeft: '0px'}}>{this.renderReturn()}</Col>
-                    <Col sm={{size: 7}} style={{paddingLeft: '14px'}}>{this.renderCriteriaMessages()}</Col>
-                  </Row>
-                </div>
-              </FormGroup>
-            </Col>
-            <Col sm="3" style={{position: 'relative'}}>{this.renderAmount()}</Col>
+            {productsContent}
           </Row>
         </FormGroup>
       );
